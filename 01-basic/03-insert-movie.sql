3d128f7e40cabf09762a805d7bee97a72fbfe6ff
valentinaojz <valentina.ojeda@soymas.cl>
2023-09-08 14:58:46 -0300

    enter-more-movies

--- 01-basic/03-insert-movie.sql
+++ 01-basic/03-insert-movie.sql
@@ -1,6 +1,21 @@
 \c movies 
-INSERT INTO movies (genere,title,year,director) VALUES(
- 'terror', 'los pitufos',2011,'gargamel' );
- INSERT INTO movies (genere,title,year,director) VALUES(
- 'terror', 'Halloween',1978,'Jhon Carpenter ' );
- 
+INSERT INTO movies (genere,title,year,director) VALUES('Animation', 'the smurfs',2011,'Gargamel');
+
+INSERT INTO movies (genere,title,year,director) VALUES('Slasher', 'Halloween',1978,'Jhon Carpenter');
+
+INSERT INTO movies (genere,title,year,director) VALUES('Gore,Horror', 'The texas chain saw massacre',1974,'Tobe Hooper');
+
+INSERT INTO movies (genere,title,year,director) VALUES('Animation','Frozen',2013,'Olaf');
+
+INSERT INTO movies (genere,title,year,director) VALUES('Science fiction', 'Back to the future',1985,'Einstein TheDog');
+INSERT INTO movies (genere,title,year,director) VALUES('Fantasy', 'Corpse bride',2005,'Tim Burton');
+
+INSERT INTO movies (genere,title,year,director) VALUES('Romantic comedy', 'Ghost',1990,'Jerry Zucker');
+
+INSERT INTO movies (genere,title,year,director) VALUES('Drama infantil', 'Iron Giant',1999,'Brad Bird');
+
+INSERT INTO movies (genere,title,year,director) VALUES('Action', 'Journey to the Center of the Earth',2008,'Brendan Fraser');
+
+INSERT INTO movies (genere,title,year,director) VALUES('Aventure', 'Paw patrol: The movie',2021,'Rubble');
+
+INSERT INTO movies (genere,title,year,director) VALUES('Adventure comedy', 'SpongeBob SquarePants:TheMovie',2004,'Calamardo');
